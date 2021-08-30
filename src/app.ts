47f75df188e56927bd727a4cc66ba4e8b88603b7
fluffypill0w <77161947+fluffypill0w@users.noreply.github.com>
2021-08-30 11:11:33 +0200

    Enabled slack Socket mode

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,9 @@ var lastCreationDate = 0;
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  logLevel: LogLevel.DEBUG
+  logLevel: LogLevel.DEBUG,
+  socketMode:true,
+  appToken: process.env.SLACK_APP_TOKEN
 });
 
 // App needs to listen for new StackExchange questions by tag
